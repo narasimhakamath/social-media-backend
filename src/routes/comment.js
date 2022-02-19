16f752b82014088d53d45c81128007b9114f7a6c
Narasimha Kamath <narasimhakamath@outlook.com>
2022-02-19 13:09:26 +0530

    - Created a GET API: /comments/:postID to get the latest list of comments added by users.

--- src/routes/comment.js
+++ src/routes/comment.js
@@ -25,4 +25,41 @@ router.post('/comments', authentication, async (req, res) => {
 	}
 });
 
+router.get('/comments/:postID', authentication, async (req, res) => {
+	const postID = req['params']['postID'];
+
+	const page = parseInt(req['query']['page'], 10) || 1;
+	const limit = parseInt(req['query']['limit'], 10) || 5;
+	const startIndex = (page - 1) * limit;
+	const endIndex = page * limit;
+
+	const queryCondition = {postID};
+
+	const total = await Comment.countDocuments(queryCondition);
+	const pagination = new Object();
+	if(startIndex > 0)
+		pagination['previousPage'] = page - 1;
+	if(endIndex < total)
+		pagination['nextPage'] = page + 1;
+
+	const comments = await Comment.find(queryCondition)
+		.select('_id comment')
+		.sort({createdAt: -1})
+		.skip(startIndex)
+		.limit(limit)
+		.populate('userID', 'name');
+
+	const commentsData = new Array();
+	for(let comment of comments) {
+		commentsData.push({
+			commentID: comment['_id'],
+			comment: comment['comment'],
+			userID: comment['userID']['_id'],
+			name: comment['userID']['name']
+		});
+	}
+
+	res.status(200).json({result: true, message: 'Fetching data successfully.', data: commentsData, pagination, total});
+});
+
 module.exports = router;
